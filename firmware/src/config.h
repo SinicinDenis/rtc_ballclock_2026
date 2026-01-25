9ce61d38f96c0ef1c9195c0f3074373210aa2cfd
SinicinDenis <110406019+SinicinDenis@users.noreply.github.com>
2026-01-25 09:42:23 +0300

    25.01.26
    25.01.26

---
+++ firmware/src/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define PROJECT_VER "1.15"
+#define PROJECT_NAME "Часы Пинг-Понг🎾"
+#define PROJECT_URL "AlexGyver/BallClock/main/project.json"
+
+#define LED_PIN 32
+#define PHOTO_PIN 33
